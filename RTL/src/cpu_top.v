0998ea5d4f452533ae2e56769f63f225399a1f09
mjalbert99 <mjalbert@ucsd.edu>
2026-06-02 13:30:51 -0700

    changed format

--- RTL/src/cpu_top.v
+++ RTL/src/cpu_top.v
@@ -1,4 +1,3 @@
-// ADD LW STR BNE INTO PROGRAM
 module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W = 4, parameter REG_N = 16, 
                  parameter MEM_N = 256, parameter OP_W = 5, parameter IMMD_W = 8, parameter DATA_W = 16)
 (
@@ -6,44 +5,32 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
     output signed[DATA_W-1:0] cpu_result,
     output valid_wb
 );
-    // VALID operation across stages
     wire valid_decode, valid_exe, valid_mem, br_en;
-
-    // Fetch signals
+    
     wire[ADDR_W-1:0] pc_index;
     wire[INSTR_W-1:0] instr, instr_decode;
 
-    // Control signals
     wire[OP_W-1:0] op_code, op_code_exe;
     wire DAT_OUT, DAT_IN, REG_IN, EN, DAT_OUT_exe, DAT_IN_exe, REG_IN_exe, EN_exe, DAT_IN_mem, DAT_OUT_mem, REG_IN_mem, REG_IN_wb;
-
-    // register addrs, register vals, immd, and dest register dest
+    
     wire[REG_W-1:0] a_idx, b_idx, c_idx, a_idx_exe, b_idx_exe, c_idx_exe, dest_exe, dest_mem, dest_wb;
     wire signed[DATA_W-1:0] a_out, b_out, c_out, a_out_exe, b_out_exe, c_out_exe;
     wire signed[IMMD_W-1:0] immd, immd_exe;
-
-    // ALU output
+    
     wire signed[DATA_W-1:0] alu_out, alu_out_mem; 
-    wire[DATA_W-1:0] str_addr, lw_addr, str_addr_mem, lw_addr_mem; 
-
-    // MEM output
+    wire[DATA_W-1:0] str_addr, lw_addr, str_addr_mem, lw_addr_mem;
+    
     wire signed[DATA_W-1:0] dat_out;
-
-    // WB data
     wire signed[DATA_W-1:0] reg_data;
-
-    // Forwarding wire for signal and outputs
+    
     wire FORWARD;
     wire signed[DATA_W-1:0] for_a_out, for_b_out, for_c_out;
-
-
     assign cpu_result = reg_data;
 
-    assign a_idx = instr_decode[(2*REG_W)+IMMD_W-1:REG_W+IMMD_W];  // Bits [16:13]
-    assign b_idx = instr_decode[(REG_W*2)-1:REG_W];                // Bits [7:4]
-    assign c_idx = instr_decode[REG_W-1:0];                        // Bits [3:0]
-    assign immd =  instr_decode[REG_W+IMMD_W-1:REG_W];             // Bits [12:4]
-
+    assign a_idx = instr_decode[(2*REG_W)+IMMD_W-1:REG_W+IMMD_W];
+    assign b_idx = instr_decode[(REG_W*2)-1:REG_W];
+    assign c_idx = instr_decode[REG_W-1:0];
+    assign immd  = instr_decode[REG_W+IMMD_W-1:REG_W];
 
     PC #(.ADDR_W(ADDR_W)) pc(
         .en(en_cpu),
@@ -54,12 +41,10 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .index(pc_index)
     );
 
-
     INSTR_MEM #(.ADDR_W(ADDR_W), .INSTR_W(INSTR_W)) instr_mem (
         .index(pc_index),
         .instr(instr)
-    ); 
-
+    );
 
     fetch_decode_pipe #(.INSTR_W(INSTR_W), .ADDR_W(ADDR_W)) ftp(
         .clk(clk),
@@ -72,7 +57,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .valid_decode(valid_decode)
     );
 
-
     CONTROLLER #(.INSTR_W(INSTR_W), .OP_W(OP_W)) controller(
         .instr(instr_decode),
         .DAT_OUT(DAT_OUT),
@@ -81,7 +65,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .EN(EN),
         .op_code(op_code)
     );
-
 
     REG_FILE #(.DATA_W(DATA_W), .REG_N(REG_N), .REG_W(REG_W)) reg_file(
         .clk(clk),
@@ -97,8 +80,7 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .c_out(c_out)
     );
 
-
-    decode_exe_pipe #(.DATA_W(DATA_W), .OP_W(OP_W), .IMMD_W(IMMD_W), .REG_W(REG_W))dep(
+    decode_exe_pipe #(.DATA_W(DATA_W), .OP_W(OP_W), .IMMD_W(IMMD_W), .REG_W(REG_W)) dep(
         .clk(clk),
         .en_cpu(en_cpu),
         .resetn(resetn),
@@ -132,7 +114,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .dest_exe(dest_exe)
     );
 
-
     ALU #(.DATA_W(DATA_W), .IMMD_W(IMMD_W), .OP_W(OP_W)) alu(
         .en(EN_exe),
         .op_code(op_code_exe),
@@ -145,7 +126,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .lw_addr(lw_addr),
         .br_en(br_en)
     );
-
 
     exe_mem_pipe #(.REG_W(REG_W), .DATA_W(DATA_W)) emp(
         .clk(clk),
@@ -169,7 +149,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .valid_mem(valid_mem)
     );
 
-
     DAT_MEM #(.DATA_W(DATA_W), .MEM_N(MEM_N)) dat_mem(
         .clk(clk),
         .wr_en(DAT_IN_mem),
@@ -179,7 +158,6 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .data_in(alu_out_mem),
         .out(dat_out)
     );
-
 
     mem_wb_pipe #(.REG_W(REG_W), .DATA_W(DATA_W)) mwp(
         .clk(clk),
@@ -197,27 +175,29 @@ module cpu_top #(parameter ADDR_W = 16, parameter INSTR_W = 21, parameter REG_W 
         .reg_data(reg_data)
     );
 
-
     forwarding_unit #(.REG_W(REG_W), .DATA_W(DATA_W), .OP_W(OP_W)) forward(
+        .clk(clk),
+        .resetn(resetn),
+        .en_cpu(en_cpu),
+        .br_en(br_en),
+        .a_idx(a_idx),
+        .b_idx(b_idx),
+        .c_idx(c_idx),
+        .op_code(op_code),
+        .valid_exe(valid_exe),
+        .REG_IN_exe(REG_IN_exe),
+        .dest_exe(dest_exe),
         .valid_mem(valid_mem),
-        .valid_wb(valid_wb),
         .REG_IN_mem(REG_IN_mem),
-        .REG_IN_wb(REG_IN_wb),
-        .a_idx(a_idx_exe),
-        .b_idx(b_idx_exe),
-        .c_idx(c_idx_exe),
+        .dest_mem(dest_mem),
         .a_out_exe(a_out_exe),
         .b_out_exe(b_out_exe),
         .c_out_exe(c_out_exe),
-        .dest_mem(dest_mem),
-        .dest_wb(dest_wb),
         .alu_out_mem(alu_out_mem),
         .reg_data(reg_data),
-        .op_code(op_code_exe),
         .for_a_out(for_a_out),
         .for_b_out(for_b_out),
         .for_c_out(for_c_out),
         .FORWARD(FORWARD)
     );
-
 endmodule
--- RTL/src/forwarding_unit.v
+++ RTL/src/forwarding_unit.v
@@ -1,30 +1,66 @@
-module forwarding_unit #(parameter REG_W = 4, parameter DATA_W = 16, parameter OP_W = 5)(
-    input valid_mem, valid_wb,
-    input REG_IN_mem, REG_IN_wb,
-    input[REG_W-1:0] a_idx, b_idx, c_idx,
-    input signed[DATA_W-1:0] a_out_exe, b_out_exe, c_out_exe,
-    input[REG_W-1:0]  dest_mem, dest_wb,
-    input signed[DATA_W-1:0] alu_out_mem, reg_data,
-    input[OP_W-1:0] op_code,
-    output signed[DATA_W-1:0] for_a_out, for_b_out, for_c_out,
+module forwarding_unit #(parameter REG_W = 4, parameter DATA_W = 16, parameter OP_W = 5
+)(
+    input clk, resetn, en_cpu, br_en,
+    input [REG_W-1:0] a_idx, b_idx, c_idx,
+    input [OP_W-1:0] op_code,
+    input valid_exe, REG_IN_exe,
+    input [REG_W-1:0] dest_exe,
+    input valid_mem, REG_IN_mem,
+    input [REG_W-1:0] dest_mem,
+    input signed [DATA_W-1:0] a_out_exe, b_out_exe, c_out_exe,
+    input signed [DATA_W-1:0] alu_out_mem, reg_data,
+    output signed [DATA_W-1:0] for_a_out, for_b_out, for_c_out,
     output FORWARD
 );
 
-    wire mem_wr = valid_mem && REG_IN_mem;
-    wire wb_wr  = valid_wb  && REG_IN_wb; 
+    wire a_match_exe = valid_exe && REG_IN_exe && (a_idx == dest_exe);
+    wire a_match_mem = valid_mem && REG_IN_mem && (a_idx == dest_mem);
+    
+    wire b_match_exe = valid_exe && REG_IN_exe && (b_idx == dest_exe);
+    wire b_match_mem = valid_mem && REG_IN_mem && (b_idx == dest_mem);
+    
+    wire is_op20 = (op_code == 5'd20);
+    wire c_match_exe = is_op20 && valid_exe && REG_IN_exe && (c_idx == dest_exe);
+    wire c_match_mem = is_op20 && valid_mem && REG_IN_mem && (c_idx == dest_mem);
 
-    assign FORWARD = (   (mem_wr && a_idx == dest_mem) || (wb_wr && a_idx == dest_wb) ||
-                         (mem_wr && b_idx == dest_mem) || (wb_wr && b_idx == dest_wb) ||
-                         (op_code == 20 && mem_wr && c_idx == dest_mem) ||
-                         (op_code == 20 && wb_wr  && c_idx == dest_wb)
-    );
+    reg sel_a_mem, sel_a_wb;
+    reg sel_b_mem, sel_b_wb;
+    reg sel_c_mem, sel_c_wb;
 
-    assign for_a_out = (mem_wr && a_idx == dest_mem) ? alu_out_mem :
-                       (wb_wr  && a_idx == dest_wb)  ? reg_data    : a_out_exe;
+    assign FORWARD = sel_a_mem || sel_a_wb || 
+                     sel_b_mem || sel_b_wb ||
+                     sel_c_mem || sel_c_wb;
 
-    assign for_b_out = (mem_wr && b_idx == dest_mem) ? alu_out_mem :
-                       (wb_wr  && b_idx == dest_wb)  ? reg_data    : b_out_exe;
+    assign for_a_out = sel_a_mem ? alu_out_mem :
+                       sel_a_wb  ? reg_data    : a_out_exe;
 
-    assign for_c_out = (op_code == 20 && mem_wr && c_idx == dest_mem) ? alu_out_mem :
-                       (op_code == 20 && wb_wr  && c_idx == dest_wb)  ? reg_data    : c_out_exe;
+    assign for_b_out = sel_b_mem ? alu_out_mem :
+                       sel_b_wb  ? reg_data    : b_out_exe;
+
+    assign for_c_out = sel_c_mem ? alu_out_mem :
+                       sel_c_wb  ? reg_data    : c_out_exe;
+                       
+    always @(posedge clk or negedge resetn) begin
+        if (!resetn) begin
+            sel_a_mem <= 1'b0; sel_a_wb <= 1'b0;
+            sel_b_mem <= 1'b0; sel_b_wb <= 1'b0;
+            sel_c_mem <= 1'b0; sel_c_wb <= 1'b0;
+        end   
+        else if (br_en) begin
+            sel_a_mem <= 1'b0; sel_a_wb <= 1'b0;
+            sel_b_mem <= 1'b0; sel_b_wb <= 1'b0;
+            sel_c_mem <= 1'b0; sel_c_wb <= 1'b0;
+        end
+        else if (en_cpu) begin
+            sel_a_mem <= a_match_exe;
+            sel_a_wb  <= !a_match_exe && a_match_mem;
+            
+            sel_b_mem <= b_match_exe;
+            sel_b_wb  <= !b_match_exe && b_match_mem;
+            
+            sel_c_mem <= c_match_exe;
+            sel_c_wb  <= !c_match_exe && c_match_mem;
+        end
+    end
+
 endmodule
